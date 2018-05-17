6f57cd42a928bb41ef9aea2fd62e1662cf200c93
sam <sam@silver42.karoo.co.uk>
2018-05-17 02:46:51 +0100

    Minor tweaks

--- extension/chartboost/Chartboost.hx
+++ extension/chartboost/Chartboost.hx
@@ -100,46 +100,46 @@ import lime.system.JNI;
 		return JNI.createStaticMethod(packageName, jniMethod, jniSignature);
 	}
 	
-	private static var close_impression:Dynamic = bindJNI("closeImpression", "()V");
+	private static var close_impression = bindJNI("closeImpression", "()V");
 	
-	private static var set_listener:Dynamic = bindJNI("setListener", "(Lorg/haxe/lime/HaxeObject;)V");
-	private static var show_interstitial:Dynamic = bindJNI("showInterstitial", "(Ljava/lang/String;)V");
-	private static var cache_interstitial:Dynamic = bindJNI("cacheInterstitial", "(Ljava/lang/String;)V");
-	private static var has_interstitial:Dynamic = bindJNI("hasInterstitial", "(Ljava/lang/String;)Z");
-	private static var show_rewarded_video:Dynamic = bindJNI("showRewardedVideo", "(Ljava/lang/String;)V");
-	private static var cache_rewarded_video:Dynamic = bindJNI("cacheRewardedVideo", "(Ljava/lang/String;)V");
-	private static var has_rewarded_video:Dynamic = bindJNI("hasRewardedVideo", "(Ljava/lang/String;)Z");
-	private static var is_any_view_visible:Dynamic = bindJNI("isAnyViewVisible", "()Z");
-	private static var set_custom_id:Dynamic = bindJNI("setCustomId", "(Ljava/lang/String;)V");
-	private static var get_custom_id:Dynamic= bindJNI("getCustomId", "()Ljava/lang/String;");
-	private static var set_should_request_interstitials_in_first_session:Dynamic = bindJNI("setShouldRequestInterstitialsInFirstSession", "(Z)V");
-	private static var get_auto_cache_ads:Dynamic = bindJNI("getAutoCacheAds", "()Z");
-	private static var set_auto_cache_ads:Dynamic = bindJNI("setAutoCacheAds", "(Z)V");
-	private static var set_should_prefetch_video_content:Dynamic = bindJNI("setShouldPrefetchVideoContent", "(Z)V");
-	private static var get_sdk_version:Dynamic = bindJNI("getSDKVersion", "()Ljava/lang/String;");
-	private static var set_should_hide_system_ui:Dynamic = bindJNI("setShouldHideSystemUI", "(Z)V");
-	private static var restrict_data_collection:Dynamic = bindJNI("restrictDataCollection", "(Z)V");
+	private static var set_listener = bindJNI("setListener", "(Lorg/haxe/lime/HaxeObject;)V");
+	private static var show_interstitial = bindJNI("showInterstitial", "(Ljava/lang/String;)V");
+	private static var cache_interstitial = bindJNI("cacheInterstitial", "(Ljava/lang/String;)V");
+	private static var has_interstitial = bindJNI("hasInterstitial", "(Ljava/lang/String;)Z");
+	private static var show_rewarded_video = bindJNI("showRewardedVideo", "(Ljava/lang/String;)V");
+	private static var cache_rewarded_video = bindJNI("cacheRewardedVideo", "(Ljava/lang/String;)V");
+	private static var has_rewarded_video = bindJNI("hasRewardedVideo", "(Ljava/lang/String;)Z");
+	private static var is_any_view_visible = bindJNI("isAnyViewVisible", "()Z");
+	private static var set_custom_id = bindJNI("setCustomId", "(Ljava/lang/String;)V");
+	private static var get_custom_id= bindJNI("getCustomId", "()Ljava/lang/String;");
+	private static var set_should_request_interstitials_in_first_session = bindJNI("setShouldRequestInterstitialsInFirstSession", "(Z)V");
+	private static var get_auto_cache_ads = bindJNI("getAutoCacheAds", "()Z");
+	private static var set_auto_cache_ads = bindJNI("setAutoCacheAds", "(Z)V");
+	private static var set_should_prefetch_video_content = bindJNI("setShouldPrefetchVideoContent", "(Z)V");
+	private static var get_sdk_version = bindJNI("getSDKVersion", "()Ljava/lang/String;");
+	private static var set_should_hide_system_ui = bindJNI("setShouldHideSystemUI", "(Z)V");
+	private static var restrict_data_collection = bindJNI("restrictDataCollection", "(Z)V");
 	#end
 	
 	#if ios
-	private static var set_listener:Object->Void = PrimeLoader.load("set_listener", "ov");
-	private static var show_interstitial:String->Void = PrimeLoader.load("show_interstitial", "sv");
-	private static var cache_interstitial:String->Void = PrimeLoader.load("cache_interstitial", "sv");
-	private static var has_interstitial:String->Bool = PrimeLoader.load("has_interstitial", "sb");
-	private static var show_rewarded_video:String->Void = PrimeLoader.load("show_rewarded_video", "sv");
-	private static var cache_rewarded_video:String->Void = PrimeLoader.load("cache_rewarded_video", "sv");
-	private static var has_rewarded_video:String->Bool = PrimeLoader.load("has_rewarded_video", "sb");
-	private static var is_any_view_visible:Void->Void = PrimeLoader.load("is_any_view_visible", "v");
-	private static var set_custom_id:String->Void = PrimeLoader.load("set_custom_id", "sv");
-	private static var get_custom_id:Void->String = PrimeLoader.load("get_custom_id", "s");
-	private static var set_should_request_interstitials_in_first_session:Bool->Void = PrimeLoader.load("set_should_request_interstitials_in_first_session", "bv");
-	private static var get_auto_cache_ads:Void->Bool = PrimeLoader.load("get_auto_cache_ads", "b");
-	private static var set_auto_cache_ads:Bool->Void = PrimeLoader.load("set_auto_cache_ads", "bv");
-	private static var set_should_prefetch_video_content:Bool->Void = PrimeLoader.load("set_should_prefetch_video_content", "bv");
-	private static var get_sdk_version:Void->String = PrimeLoader.load("get_sdk_version", "s");
-	private static var set_should_hide_system_ui:Bool->Void = PrimeLoader.load("set_status_bar_behavior", "bv");
-	private static var set_muted:Bool->Void = PrimeLoader.load("set_muted", "bv");
-	private static var restrict_data_collection:Bool->Void = PrimeLoader.load("restrict_data_collection", "bv");
+	private static var set_listener = PrimeLoader.load("set_listener", "ov");
+	private static var show_interstitial = PrimeLoader.load("show_interstitial", "sv");
+	private static var cache_interstitial = PrimeLoader.load("cache_interstitial", "sv");
+	private static var has_interstitial = PrimeLoader.load("has_interstitial", "sb");
+	private static var show_rewarded_video = PrimeLoader.load("show_rewarded_video", "sv");
+	private static var cache_rewarded_video = PrimeLoader.load("cache_rewarded_video", "sv");
+	private static var has_rewarded_video = PrimeLoader.load("has_rewarded_video", "sb");
+	private static var is_any_view_visible = PrimeLoader.load("is_any_view_visible", "v");
+	private static var set_custom_id = PrimeLoader.load("set_custom_id", "sv");
+	private static var get_custom_id = PrimeLoader.load("get_custom_id", "s");
+	private static var set_should_request_interstitials_in_first_session = PrimeLoader.load("set_should_request_interstitials_in_first_session", "bv");
+	private static var get_auto_cache_ads = PrimeLoader.load("get_auto_cache_ads", "b");
+	private static var set_auto_cache_ads = PrimeLoader.load("set_auto_cache_ads", "bv");
+	private static var set_should_prefetch_video_content = PrimeLoader.load("set_should_prefetch_video_content", "bv");
+	private static var get_sdk_version = PrimeLoader.load("get_sdk_version", "s");
+	private static var set_should_hide_system_ui = PrimeLoader.load("set_status_bar_behavior", "bv");
+	private static var set_muted = PrimeLoader.load("set_muted", "bv");
+	private static var restrict_data_collection = PrimeLoader.load("restrict_data_collection", "bv");
 	#end
 }
 
