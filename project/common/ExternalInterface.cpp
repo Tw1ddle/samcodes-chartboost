4421cf54134271e9d1c7290b26fcfb1f44c0043d
sam <sam@silver42.karoo.co.uk>
2018-05-18 22:58:43 +0100

    More typos (about time I got a working mac)

--- project/common/ExternalInterface.cpp
+++ project/common/ExternalInterface.cpp
@@ -17,7 +17,7 @@ using namespace samcodeschartboost;
 
 AutoGCRoot* chartboostEventHandle = 0;
 
-void set_listener(value onEvent)
+void samcodeschartboost_set_listener(value onEvent)
 {
 	if(chartboostEventHandle == 0) {
 		chartboostEventHandle = new AutoGCRoot(onEvent);
@@ -27,97 +27,97 @@ void set_listener(value onEvent)
 }
 DEFINE_PRIME1v(samcodeschartboost_set_listener);
 
-void show_interstitial(HxString location)
+void samcodeschartboost_show_interstitial(HxString location)
 {
 	showInterstitial(location.c_str());
 }
 DEFINE_PRIME1v(samcodeschartboost_show_interstitial);
 
-void cache_interstitial(HxString location)
+void samcodeschartboost_cache_interstitial(HxString location)
 {
 	cacheInterstitial(location.c_str());
 }
 DEFINE_PRIME1v(samcodeschartboost_cache_interstitial);
 
-bool has_interstitial(HxString location)
+bool samcodeschartboost_has_interstitial(HxString location)
 {
 	return hasInterstitial(location.c_str());
 }
 DEFINE_PRIME1(samcodeschartboost_has_interstitial);
 
-void show_rewarded_video(HxString location)
+void samcodeschartboost_show_rewarded_video(HxString location)
 {
 	showRewardedVideo(location.c_str());
 }
 DEFINE_PRIME1v(samcodeschartboost_show_rewarded_video);
 
-void cache_rewarded_video(HxString location)
+void samcodeschartboost_cache_rewarded_video(HxString location)
 {
 	cacheRewardedVideo(location.c_str());
 }
 DEFINE_PRIME1v(samcodeschartboost_cache_rewarded_video);
 
-bool has_rewarded_video(HxString location)
+bool samcodeschartboost_has_rewarded_video(HxString location)
 {
 	return hasRewardedVideo(location.c_str());
 }
 DEFINE_PRIME1samcodeschartboost_(has_rewarded_video);
 
-bool is_any_view_visible()
+bool samcodeschartboost_is_any_view_visible()
 {
 	return isAnyViewVisible();
 }
 DEFINE_PRIME0(samcodeschartboost_is_any_view_visible);
 
-void set_custom_id(HxString id)
+void samcodeschartboost_set_custom_id(HxString id)
 {
 	setCustomId(id.c_str());
 }
 DEFINE_PRIME1v(samcodeschartboost_set_custom_id);
 
-HxString get_custom_id()
+HxString samcodeschartboost_get_custom_id()
 {
 	return HxString(getCustomId());
 }
 DEFINE_PRIME0(samcodeschartboost_get_custom_id);
 
-void set_should_request_interstitials_in_first_session(bool shouldRequest)
+void samcodeschartboost_set_should_request_interstitials_in_first_session(bool shouldRequest)
 {
 	setShouldRequestInterstitialsInFirstSession(shouldRequest);
 }
 DEFINE_PRIME1v(samcodeschartboost_set_should_request_interstitials_in_first_session);
 
-bool get_auto_cache_ads()
+bool samcodeschartboost_get_auto_cache_ads()
 {
 	return getAutoCacheAds();
 }
 DEFINE_PRIME0(samcodeschartboost_get_auto_cache_ads);
 
-void set_auto_cache_ads(bool autoCache)
+void samcodeschartboost_set_auto_cache_ads(bool autoCache)
 {
 	setAutoCacheAds(autoCache);
 }
 DEFINE_PRIME1v(samcodeschartboost_set_auto_cache_ads);
 
-void set_should_prefetch_video_content(bool shouldPrefetch)
+void samcodeschartboost_set_should_prefetch_video_content(bool shouldPrefetch)
 {
 	setShouldPrefetchVideoContent(shouldPrefetch);
 }
 DEFINE_PRIME1v(samcodeschartboost_set_should_prefetch_video_content);
 
-HxString get_sdk_version()
+HxString samcodeschartboost_get_sdk_version()
 {
 	return HxString(getSDKVersion());
 }
 DEFINE_PRIME0(samcodeschartboost_get_sdk_version);
 
-void set_status_bar_behavior(bool shouldHide)
+void samcodeschartboost_set_status_bar_behavior(bool shouldHide)
 {
 	setStatusBarBehavior(shouldHide);
 }
 DEFINE_PRIME1v(samcodeschartboost_set_status_bar_behavior);
 
-void set_muted(bool mute)
+void samcodeschartboost_set_muted(bool mute)
 {
 	setMuted(mute);
 }
